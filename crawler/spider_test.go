3ca26822446680dba685fe136253cdf869c1c790
Chang Sau Sheong <sausheong@gmail.com>
2014-05-22 12:48:30 +0800

    added stopwords

--- crawler/spider_test.go
+++ crawler/spider_test.go
@@ -1,23 +1,23 @@
 package main
 
 import "testing"
-import "io/ioutil"
-import "strings"
+// import "io/ioutil"
+// import "strings"
 // import "fmt"
 
 
-func Test_Scrub(t *testing.T) {
-  b, err := ioutil.ReadFile("test_urls.txt")
-  if err != nil { 
-   t.Errorf("Error is %v", err) 
-  }
-  for _, line := range strings.Split(string(b), "\n") {
-    _, err := scrub(line)
-    if err != nil {
-      t.Errorf("Error is %v", err) 
-    }
-  }  
-}
+// func Test_Scrub(t *testing.T) {
+//   b, err := ioutil.ReadFile("test_urls.txt")
+//   if err != nil { 
+//    t.Errorf("Error is %v", err) 
+//   }
+//   for _, line := range strings.Split(string(b), "\n") {
+//     _, err := scrub(line)
+//     if err != nil {
+//       t.Errorf("Error is %v", err) 
+//     }
+//   }  
+// }
 // 
 // func Test_Links(t *testing.T) {
 //   link := "https://sg.yahoo.com/?p=us"
@@ -37,9 +37,18 @@ func Test_Scrub(t *testing.T) {
 // }
 
 func Test_WordsFrom(t *testing.T) {
-  words := words_from("http://edition.cnn.com/2012/07/08/opinion/banzi-ted-open-source/index.html")
+  words := words_from("https://sg.news.yahoo.com/red-faces-french-trains-too-wide-stations-144918613.html")
   for _, val := range words {
    println(val) 
   }
+  
+}
 
+func Test_IgnoreWord(t *testing.T) {
+  if ignore("a") == false {
+    t.Errorf("a is not ignored") 
+  }
+  if ignore("nothing") == false {
+    t.Errorf("nothing is not ignored") 
+  }
 }
